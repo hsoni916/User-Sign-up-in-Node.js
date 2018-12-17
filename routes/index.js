fd27b0bfa733feb93da2e9140bede519597ce098
Harsh Patalia <pataliaharsh948@gmail.com>
2018-12-17 18:57:31 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {  });
-});
 console.log("index.js");
 var app = express();
 var Parse = require('body-parser');
 app.use(Parse.json());
 app.use(Parse.urlencoded({ extended: true }));
-
+app.use(session({secret: ' alpha/elt#izmo15/12/18.'}))
+var sess; 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	sess = req.session;
+	if(sess.Email){
+		res.redirect('/home');
+	}
+	else{
+  res.render('index', {  });
+}
+});
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/ListOfUsers");
@@ -21,13 +29,14 @@ var userschema = new mongoose.Schema({
  Passkey: String
 });
 var user = mongoose.model("User", userschema);
-router.post('/loginUser', function(req,res, next) {
+router.post('/loginUser', function(req,res) {
 	var newuser = new user(req.body);
+	sess = req.session;
 	newuser.save()
-	.then(item=>{
-		var json = req.params;
-       res.send(json);
-	})
+	.then(item=>{  
+			sess.Email = req.body.Email;
+			res.redirect('/home');
+		})
 	.catch(err=> {
 		res.status(400).send("Failure");
 	})
