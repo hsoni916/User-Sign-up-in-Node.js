e62abd37dc597c4f5c4761e725bc97732c89cc1b
Harsh Patalia <pataliaharsh948@gmail.com>
2018-12-17 18:59:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var homeRouter = require('./routes/home');
 var app = express();
 
 // view engine setup
@@ -16,14 +15,13 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use (express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
+app.use('/home',homeRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
